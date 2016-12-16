16cb0eb8efd2acda04806f24b606adcf42d00d4a
LaurenSky <ljherrer@gmail.com>
2016-12-16 15:09:01 -0800

    moved app.js to src folder from app folder

--- src/app.js
+++ src/app.js
@@ -1,5 +1,5 @@
 // import Application from 'app/models/application'; **** DO WE NEED THIS????
-import ApplicationView from 'app/views/app_view';
+// import ApplicationView from 'app/views/app_view';
 import $ from 'jquery';
 
 
