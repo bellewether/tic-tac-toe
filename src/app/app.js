5941707bc577653757c9f3b3ac750481f1e95d46
LaurenSky <ljherrer@gmail.com>
2016-12-16 15:06:56 -0800

    file structure set up

---
+++ src/app/app.js
@@ -0,0 +1,8 @@
+// import Application from 'app/models/application'; **** DO WE NEED THIS????
+import ApplicationView from 'app/views/app_view';
+import $ from 'jquery';
+
+
+$(document).ready(function() {
+
+});
---
+++ src/app/models/game.js
@@ -0,0 +1,136 @@
+var Game = function() {
+  this.board = new GameBoard();
+  this.player1 = new Player1();
+  this.player2 = new Player2();
+  this.gameCounter = true;
+  this.turnCounter = 0;
+  this.winner = null;
+  console.log(this.board);
+  // console.log(this.player1);
+  // console.log(this.player2);
+};
+
+Game.prototype.playTurn = function(row, column) {
+  if(this.winner !== null) {
+    console.log("Game is Over " + this.winner.name + " won.");
+    return "Game is Over " + this.winner.name + " won.";
+  } else {
+    // console.log(this.whichPlayer());
+    var player = this.whichPlayer();
+    // console.log(this.valid(row, column));
+    if (this.valid(row, column)) {
+      this.board.gameBoard[row][column] = player.marker;
+
+      if (player == this.player1) {
+        this.gameCounter = false;
+        this.turnCounter++ ;
+      } else {
+        this.gameCounter = true;
+        this.turnCounter++ ;
+      }
+
+      if(this.turnCounter >= 5) {
+        if(this.board.hasWon() === true) {
+          console.log(player + " you're the Winner!!!");
+          this.winner = player;
+          return player.name;
+        } else if(this.board.hasWon() === "tie") {
+          console.log("Cat's Game, it's a tie.");
+          // return "Cat's Game.";
+        }
+      }
+
+    } else {
+      console.log("That position is already taken, go Again");
+    }
+    console.log(this.board);
+    console.log("who's turn: " + this.gameCounter);
+    console.log("round number: " + this.turnCounter);
+  }
+};
+
+Game.prototype.whichPlayer = function() {
+  if (this.gameCounter === true) {
+    return this.player1;
+  } else {
+    return this.player2;
+  }
+};
+
+Game.prototype.valid = function(row,column) {
+  if((row > 2) || (column > 2)) {
+    console.log(row + "," + column + " is not a valid location");
+    return false;
+  } else {
+    var locationValue = this.board.gameBoard[row][column];
+    console.log("in valid, location value = " + locationValue);
+    if (locationValue != 'X' && locationValue != 'O') {
+      return true;
+    } else {
+      return false;
+    }
+  }
+};
+
+var GameBoard = function() {
+  this.gameBoard = [];
+  this.gameBoard[0] = [ null, null, null];
+  this.gameBoard[1] = [ null, null, null];
+  this.gameBoard[2] = [ null, null, null];
+};
+
+GameBoard.prototype.hasWon = function() {
+  // var board = this.gameBoard;
+  var row0 = this.gameBoard[0];
+  var row1 = this.gameBoard[1];
+  var row2 = this.gameBoard[2];
+
+  if ((row0[0] == row0[1]  && row0[1] == row0[2] && row0[2] !== null) ||
+      (row1[0] == row1[1]  && row1[1] == row1[2] && row1[2] !== null) ||
+      (row2[0] == row2[1]  && row2[1] == row2[2] && row2[2] !== null)) {
+    console.log("Winner in a row");
+    return true;
+  } else if ((row0[0] == row1[0] && row1[0] == row2[0] && row2[0] !== null) ||
+              (row0[1] == row1[1] && row1[1] == row2[1] && row2[1] !== null) ||
+              (row0[2] == row1[2] && row1[2] == row2[2] && row2[2] !== null)) {
+    console.log("Winner in a column");
+    return true;
+  } else if ((row0[0] == row1[1] && row1[1] == row2[2] && row2[2] !== null) || (row0[2] == row1[1] && row1[1] == row2[0] && row2[0] !== null)) {
+      console.log("Winner in a diagonal");
+      return true;
+  } else {
+    if(this.aTie()){
+      return "tie";
+    } else {
+      return false;
+    }
+  }
+};
+
+GameBoard.prototype.aTie = function() {
+  var row0 = this.gameBoard[0];
+  var row1 = this.gameBoard[1];
+  var row2 = this.gameBoard[2];
+
+  if((row0.includes(null)) || (row1.includes(null)) || (row2.includes(null))) {
+    return false;
+  } else {
+    return true;
+  }
+};
+
+var Player1 = function() {
+  this.marker = "X";
+  this.turnCounter = true;
+  this.name = "player1";
+};
+
+var Player2 = function() {
+  this.marker = "O";
+  this.turnCounter = false;
+  this.name = "player2";
+};
+
+
+// DO NOT REMOVE THIS
+export default Game;
